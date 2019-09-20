38ef022d01e2597118393cb0e323eb08f92ef3e8
thekunalraval <43860748+thekunalraval@users.noreply.github.com>
2019-09-20 09:32:00 -0400

    Add files via upload

---
+++ whatdatatype.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+<?php
+$a="hi";
+$b=5;
+$c=1;
+echo ($a+$b . + '<br>');
+echo ($b+$a. + '<br>');
+echo ($a+$c. + '<br>');
+echo ($b+$c. + '<br>');
+echo ($c+$a. + '<br>');
+echo ($c+$b. + '<br>');
+echo ($a+$a. + '<br>');
+echo ($b+$b. + '<br>');
+echo ($c+$c. + '<br>');
+?>
+<body>
+</body>
+</html>
